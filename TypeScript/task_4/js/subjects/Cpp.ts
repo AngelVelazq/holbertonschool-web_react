2c5144c559496fd5be753d222be53aa00bccfa3c
AngelVelazq <angelvelazquez.op@gmail.com>
2025-02-05 14:05:05 -0400

    sending task4

---
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,16 @@
+/// <reference path="./Subject.ts" />
+namespace Subjects {
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
